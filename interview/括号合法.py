a16cd7365057671f62ad5d3bcf650caf817e27e9
root <guomin012809@126.com>
2020-06-23 19:10:46 +0800

    Default Changelist

--- interview/括号合法.py
+++ interview/括号合法.py
@@ -9,42 +9,43 @@
 # Input: "{[]}"
 # Output: true
 
-def isValid(str):
-    i=0 #分别表示‘()’、'[]'、'{}'
-    j=0
-    k=0
+
+def is_valid(str):
+    i = 0  # 分别表示‘()’、'[]'、'{}'
+    j = 0
+    k = 0
     for c in str:
-        if c=='(': 
-            i+=1
-        if c== ')':
-            i-=1
-            if i%2==j%2 and i%2==k%2:
+        if c == '(':
+            i += 1
+        if c == ')':
+            i -= 1
+            if i % 2 == j % 2 and i % 2 == k % 2:
                 continue
             else:
                 return False
-        if c=='[': 
-            i+=1
-        if c== ']':
-            i-=1
-            if i%2==j%2 and i%2==k%2:
+        if c == '[':
+            i += 1
+        if c == ']':
+            i -= 1
+            if i % 2 == j % 2 and i % 2 == k % 2:
                 continue
             else:
                 return False
-        if c=='{': 
-            i+=1
-        if c== '}':
-            i-=1
-            if i%2==j%2 and i%2==k%2:
+        if c == '{':
+            i += 1
+        if c == '}':
+            i -= 1
+            if i % 2 == j % 2 and i % 2 == k % 2:
                 continue
             else:
                 return False
-    
-    if i==j==k==0:
+
+    if i == j == k == 0:
         return True
     else:
         return False
 
-if __name__=='__main__':
-    str='()[]][]{}[]()['
-    print(isValid(str))
-    
+
+if __name__ == '__main__':
+    str = '()[]][]{}[]()['
+    print(is_valid(str))
--- interview/接雨水.py
+++ interview/接雨水.py
@@ -1,38 +1,44 @@
-## 大体的思路，二分法，先找到整个数组里最高的点，然后依此再找左边的最高点，依次左边向左，右边向右
+# 大体的思路，二分法，先找到整个数组里最高的点，然后依此再找左边的最高点，依次左边向左，右边向右
 
-def trap(heights):
-    sum=0
+
+def my_trap(heights):
+    water_sum = 0
     left_max = right_max = get_max(heights)
 
-    ## 左边只往左走
+    # 左边只往左走
     l_max = get_max(heights[:left_max])
-    while l_max>0:
-        for i in heights[l_max+1:left_max]:
-            sum = sum+(heights[l_max]-i)
-        left_max = l_max
-        l_max = get_max(heights[:left_max])
-    
-    ## 右边往右走
-    r_max = get_max(heights[right_max+1:])
-    while r_max<len(heights)-1:
-        for i in heights[right_max+1:r_max]:
-            sum = sum+(heights[r_max]-i)
-        right_max = r_max
-        r_max = get_max(heights[right_max+1:])
-    
-    return sum
 
+    while l_max >= 0:
+        for i in heights[l_max + 1:left_max]:
+            water_sum = water_sum + (heights[l_max] - i)
+        left_max = l_max
+        if l_max == 0:
+            break
+        l_max = get_max(heights[:left_max])
+
+    # 右边往右走
+    r_max = right_max + get_max(heights[right_max + 1:]) + 1
+
+    while r_max <= len(heights) - 1:
+        for i in heights[right_max + 1:r_max]:
+            water_sum = water_sum + (heights[r_max] - i)
+        right_max = r_max
+        if r_max == len(heights) - 1:
+            break
+        r_max = right_max + get_max(heights[right_max + 1:]) + 1
+
+    return water_sum
 
 
 def get_max(heights):
-    max =0
+    height_max = 0
     if heights:
         for i in range(len(heights)):
-            if heights[i]>=heights[max]:
-                max=i
-    return max
+            if heights[i] >= heights[height_max]:
+                height_max = i
+    return height_max
 
-if __name__ =='__main__':
-    a=[4,2,1,9,3,1,6]
-    print(trap(a))
 
+if __name__ == '__main__':
+    a = [3, 3, 2, 4, 2, 1, 9, 3, 1, 6, 4, 2, 1, 5]
+    print(my_trap(a))
