dd7b3c26bbc4d51eaec5abf9e94d8a93256ccd77
root <guomin012809@126.com>
2020-06-25 09:03:12 +0800

    面试之数列反转

--- interview/数列反转.py
+++ interview/数列反转.py
@@ -1,6 +1,13 @@
+# 单调递增数列 [1,2,3,4,5]  数列反转 如[3,4,5,1,2] ,找出数列中最小值，二分法
+
+
 def find_reversal(r_list):
     start = 0
     end = len(r_list) - 1
+    # 先判断数列是否反转，没有反转，第一个就是最小值
+    if r_list[end] > r_list[start]:
+        return start
+
     median = int((start + end) / 2)
 
     while True:
@@ -16,5 +23,5 @@ def find_reversal(r_list):
 
 
 if __name__ == "__main__":
-    my_list = [4, 5, 1, 2, 3]
+    my_list = [1, 2, 3]
     print(my_list[find_reversal(my_list)])
